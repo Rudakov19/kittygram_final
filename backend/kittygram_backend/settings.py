36f6fd80dc15051793b96f4536c31c8798e24bd3
Дмитрий Рудаков <rud@kov19.ru>
2024-02-08 17:52:39 +0300

    Пробуем передать переменные в settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$' # os.getenv('SECRET_KEY', default='key')
+SECRET_KEY = os.environ['SECRET_KEY'] #'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = False #(os.getenv('DEBUG', 'false').lower() == 'true')
+DEBUG = os.environ['DEBUG'].lower() == 'true' # False
 
-ALLOWED_HOSTS = ['158.160.78.117', '127.0.0.1', 'localhost', 'kittygramservice.ddns.net'] # os.getenv('ALLOWED_HOSTS','127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',') # ['158.160.78.117', '127.0.0.1', 'localhost', 'kittygramservice.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
