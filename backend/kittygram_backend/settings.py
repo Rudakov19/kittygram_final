a3f70cb7520b8a618b8b2ac8071bbe4d78a013f0
Дмитрий Рудаков <rud@kov19.ru>
2024-02-09 02:28:42 +0300

    Добавил что тестирование происходит из любой ветки

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='key')
 
-DEBUG = (os.getenv('DEBUG', 'false').lower() == 'true')
+DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(', ')
 
