323e3a5c5d94c575edab227e471d7b4fec8f6110
Дмитрий Рудаков <rud@kov19.ru>
2024-02-08 17:40:04 +0300

    Сайт поломался, возвращаю прежний settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='key')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$' # os.getenv('SECRET_KEY', default='key')
 
-DEBUG = (os.getenv('DEBUG', 'false').lower() == 'true')
+DEBUG = False #(os.getenv('DEBUG', 'false').lower() == 'true')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['158.160.78.117', '127.0.0.1', 'localhost', 'kittygramservice.ddns.net'] # os.getenv('ALLOWED_HOSTS','127.0.0.1,localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
